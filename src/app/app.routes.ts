0c3107cbd9c764f19aea19805466af060d078c7b
UdayShankar754 <uday.naxtre@gmail.com>
2024-08-13 14:08:19 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PageLayoutComponent } from './pages/page-layout/page-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path : '' , component : PageLayoutComponent}
+];
---
+++ src/app/pages/page-layout/page-layout.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+@Component({
+  selector: 'app-page-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet, 
+    NzIconModule, 
+    NzLayoutModule, 
+    NzMenuModule,
+  ],
+  templateUrl: './page-layout.component.html',
+  styleUrl: './page-layout.component.css'
+})
+export class PageLayoutComponent {
+  isCollapsed = false;
+
+}
