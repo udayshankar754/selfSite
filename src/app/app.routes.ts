3bb058eb5bd28ded9ce9665f08b51f0a563696c5
UdayShankar754 <uday.naxtre@gmail.com>
2024-09-03 11:43:00 +0530

    home pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageLayoutComponent } from './pages/page-layout/page-layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path : '' , component : PageLayoutComponent}
+  { path : '' , component : PageLayoutComponent , children : [
+    { path : 'home' , component : HomeComponent}
+  ]},
 ];
